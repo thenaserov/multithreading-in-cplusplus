002730b970872e3a128cfc60c89a7aa657a773ba
naserrezayi1998 <naser.rezayi.nr@gmail.com>
2023-02-27 22:56:56 +0330

    [ADD] first files

---
+++ shared data.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <thread>
+
+int main()
+{
+    std::atomic<int> count = 0;
+    const int ITERATIONS = 1000;
+
+    std::thread t1([&count]()
+    {
+        for (int i = 0; i < ITERATIONS; i++)
+        {
+            ++count;
+        }
+    });
+
+    std::thread t2([&count]()
+    {
+        for (int i = 0; i < ITERATIONS; i++)
+        {
+            ++count;
+        }
+    });
+
+    t1.join();
+    t2.join();
+
+	std::cout << count << std::endl;
+}
