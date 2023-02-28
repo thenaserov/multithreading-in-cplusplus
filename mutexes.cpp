ef8a6444a9b69bb95fd594d9f5e26fe63d51e039
naserrezayi1998 <naser.rezayi.nr@gmail.com>
2023-02-28 19:12:42 +0330

    [ADD] mutexes

--- mutexes.cpp
+++ mutexes.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int count{ 0 };
+	const int ITERATIONS = 1'000'000;
+	std::mutex mtx;
+
+	std::thread t1([&count, &mtx]() {
+		mtx.lock();
+		for (int i{ 0 }; i < ITERATIONS; i++)
+		{
+			++count;
+		}
+		mtx.unlock();
+	});
+
+	std::thread t2([&count, &mtx]() {
+		mtx.lock();
+		for (int i{ 0 }; i < ITERATIONS; i++)
+		{
+			++count;
+		}
+		mtx.unlock();
+	});
+
+	t1.join();
+	t2.join();
+
+	std::cout << count << std::endl;
 }
