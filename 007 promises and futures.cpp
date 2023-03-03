442dc55c15d0a21d7bbcef20dfe4bb431b3bffdb
naserrezayi1998 <naser.rezayi.nr@gmail.com>
2023-03-03 23:28:28 +0330

    [ADD] prmises and futures example -> as function args

---
+++ 007 promises and futures.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <thread>
+#include <future>
+#include <iomanip>
+
+void CalculatePi(int terms, std::promise<float> prms)
+{
+	float result{ 0 };
+	for (int i = 0; i <= terms; i++)
+	{
+		int sign = pow(-1, i);
+		double term = 1.0 / (i * 2 + 1);
+		result += sign * term;
+	}
+	prms.set_value(result * 4);
+}
+
+int main()
+{
+	std::promise<float> prms;
+	std::future<float> ftr = prms.get_future();
+	std::thread t1(std::ref(CalculatePi), 1'000'000'000, std::move(prms));
+	std::cout << std::setprecision(20) << ftr.get() << std::endl;
+	t1.join();
+	return 0;
+}
