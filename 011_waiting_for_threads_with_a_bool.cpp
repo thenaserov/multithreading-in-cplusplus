a39202f13bd78736822d6679915abfa401cfae8d
Naser Rezayi <54706692+naserrezayi1998@users.noreply.github.com>
2023-07-24 22:25:14 +0330

    Update 011_waiting_for_threads_with_a_bool.cpp
    [REF]

--- 011_waiting_for_threads_with_a_bool.cpp
+++ 011_waiting_for_threads_with_a_bool.cpp
@@ -2,19 +2,17 @@
 #include <thread>
 #include <chrono>
 
-int main()
-{
+int main() {
     std::atomic<bool> ready = false;
 	std::thread t1([&](){
-		std::cout << "Waiting for 2000 milli-seconds" << std::endl;
-		std::this_thread::sleep_for(std::chrono::milliseconds(2'000));
-        ready = true;
-    });
-    t1.join();
-    while(!ready)
-    {
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	    std::cout << "Waiting for 2000 milli-seconds" << std::endl;
+	    std::this_thread::sleep_for(std::chrono::milliseconds(2'000));
+            ready = true;
+        });
+    while(!ready) {
+	std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-	std::cout << "ready " << ready << std::endl;
+    std::cout << "ready " << ready << std::endl;
+    t1.join();
     return 0;
 }
