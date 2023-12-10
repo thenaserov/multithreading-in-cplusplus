10d3c5d7dd02c146564644d31caa103e5ea10ed0
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2023-12-10 02:58:00 -0800

    Create multi_core_process.cpp
    [ADD] async

---
+++ multi_core_process.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <future>
+#include <chrono>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <cmath>
+#include <iomanip>
+
+
+double calculate_pi(int terms, int start, int skip)
+{
+    double sum = 0.0;
+
+    for (int i = start; i < terms; i += skip)
+    {
+        int sign = pow(-1, i);
+        double term = 1.0 / (i * 2 + 1);
+        sum += sign * term;
+    }
+
+    return sum * 4;
+}
+
+int main()
+{
+    std::vector<std::shared_future<double>> futures;
+
+    const int CONCURRENCY = std::thread::hardware_concurrency();
+    std::cout << CONCURRENCY << std::endl;
+    for (int i = 0; i < CONCURRENCY; i++)
+    {
+        std::shared_future<double> f = std::async(std::launch::async, calculate_pi, 1E10, i, CONCURRENCY);
+        futures.push_back(f);
+    }
+
+    double sum = 0.0;
+
+    for (auto f : futures)
+    {
+        sum += f.get();
+    }
+
+    std::cout << std::setprecision(15) << "PI:  " << 3.14159 << std::endl;
+    std::cout << std::setprecision(15) << "Our PI: " << sum << std::endl;
+
+    return 0;
+}
